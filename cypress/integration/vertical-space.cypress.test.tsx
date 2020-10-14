c52693a1bba3d97ab7b0f993a34fc2bfdfcb7a93
tim <tim@glo.id.au>
2020-10-14 22:23:39 +1100

    fix import pathing

--- cypress/integration/vertical-space.cypress.test.tsx
+++ cypress/integration/vertical-space.cypress.test.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { mount } from 'cypress-react-unit-test';
 
-import { VerticalSpace } from './vertical-space.component';
+import { VerticalSpace } from '../../src/components/vertical-space/vertical-space.component';
 
 describe('VerticalSpace component', () => {
   it('works', () => {
