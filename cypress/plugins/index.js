75271683bc035b25aab4029a8835f9c20252366c
Tim Paul <tim.paul@immutable.com>
2020-10-15 11:01:51 +1100

    fix bad loaders

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,11 +1,10 @@
-// load Webpack file preprocessor that comes with this plugin
+// load file preprocessor that comes with this plugin
 // https://github.com/bahmutov/cypress-react-unit-test#install
-const preprocessor = require('cypress-react-unit-test/plugins/load-webpack')
 module.exports = (on, config) => {
-  // point at the webpack config file at the root of the project
+  // from the root of the project (folder with cypress.json file)
   config.env.webpackFilename = 'internals/webpack/webpack.cypress.config.js';
-  preprocessor(on, config)
+  require('cypress-react-unit-test/plugins/load-webpack')(on, config);
   // IMPORTANT to return the config object
   // with the any changed environment variables
-  return config
+  return config;
 }
--- internals/webpack/webpack.cypress.config.js
+++ internals/webpack/webpack.cypress.config.js
@@ -3,31 +3,5 @@ const path = require('path');
 
 module.exports = {
   ...config,
-  // resolve: {
-  //   ...config.resolve,
-  //   alias: {
-  //     '@components': path.resolve(__dirname, '../../src/components'),
-  //   },
-  // },
-  module: {
-    rules: [
-      ...config.module.rules,
-      {
-        // Include ts, tsx, js, and jsx files.
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: [
-          { loader: 'babel-loader' }, // using babel after typescript transpiles to target es6
-          {
-            loader: 'ts-loader',
-            options: {
-              logLevel: 'info',
-              // skip typechecking for speed
-              transpileOnly: true,
-            },
-          },
-        ],
-      },
-    ]
-  },
+  mode: 'development',
 };
